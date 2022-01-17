2e48a56626601a15cbfc4208c11db29b64df6c75
Jitender Chauhan <66746889+JackEngineer-coder@users.noreply.github.com>
2022-01-17 14:02:03 +0530

    Create Introduction to Binary_tree.java
    In this we have created a node class and we have also created a constructor.

---
+++ Introduction to Binary_tree.java
@@ -0,0 +1,18 @@
+import java.util.*;
+import java.io.*;
+
+public class Main{
+  public static class Node{
+     int data;
+     Node right;
+     Node left;
+     Node(int data, Node right, Node left)
+     {
+       this.data = data;
+       this.right = right;
+       this.left = left;
+  }
+
+  public static void main(String[] args) throws Exception{
+  
+}
